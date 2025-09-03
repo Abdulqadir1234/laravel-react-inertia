8b36ed1b113c1e6664945cab41ceb04e19b2a440
Abdulqadir1234 <abdulqadirnasrat@gmail.com>
2025-09-03 06:49:47 -0700

    task factory created

--- database/factories/TaskFactory.php
+++ database/factories/TaskFactory.php
@@ -10,6 +10,7 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 class TaskFactory extends Factory
 {
     /**
+     * 
      * Define the model's default state.
      *
      * @return array<string, mixed>
