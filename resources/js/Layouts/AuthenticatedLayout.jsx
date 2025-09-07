2252de0492d4d30e29205ae15a9e30b75644bdb6
Abdulqadir1234 <abdulqadirnasrat@gmail.com>
2025-09-07 03:56:24 -0700

    nav tags created for projects tasks and users

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -23,13 +23,38 @@ export default function AuthenticatedLayout({ header, children }) {
                                 </Link>
                             </div>
 
-                            <div className="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                            <div className=" space-x-8 sm:-my-px sm:ms-10 sm:flex">
                                 <NavLink
                                     href={route('dashboard')}
                                     active={route().current('dashboard')}
                                 >
                                     Dashboard
                                 </NavLink>
+
+                                <NavLink
+                                    href={route('project.index')}
+                                    active={route().current('project.index')}
+                                >
+                                    Projects
+                                </NavLink>
+
+                                <NavLink
+                                    href={route('project.index')}
+                                    active={route().current('project.index')}
+                                >
+                                    Projects
+                                </NavLink>
+
+                                <NavLink
+                                    href={route('user.index')}
+                                    active={route().current('user.index')}
+                                >
+                                    Users
+                                </NavLink>
+                               
+
+                              
+                                
                             </div>
                         </div>
 
